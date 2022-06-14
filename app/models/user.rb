fffdfffb490cde1e6a714c5f1daaae9e3628123a
hachiko38101 <theo21011011@gmail.com>
2022-06-14 12:18:07 +0900

    バリデーションの修正（分岐）追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   katakana = /\A[ァ-ヶー－]+\z/
 
 
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}, allow_blank: true
   validates :nickname, presence: true
   validates :first_name_kanji,presence: true
   validates :first_name_kanji, format: { with: zenkaku, message: 'Input full-width characters' }, allow_blank: true
