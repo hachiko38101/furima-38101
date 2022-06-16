b3e2b77a463e553ef050865116df86a3e3b79d34
hachiko38101 <theo21011011@gmail.com>
2022-06-16 18:54:36 +0900

    結合テストコードの記述

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,2 +1,31 @@
 module SignInSupport
+  def basic_pass(path)
+    username = ENV['BASIC_AUTH_USER']
+    password = ENV['BASIC_AUTH_PASSWORD']
+    visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+  end
+
+  def sign_in(user)
+    @user = FactoryBot.build(:user)
+    basic_pass root_path
+    visit root_path
+    expect(page).to have_content('新規登録')
+    expect(page).to have_content('ログイン')
+    visit new_user_registration_path
+    fill_in 'nickname', with: @user.nickname
+    fill_in 'email', with: @user.email
+    fill_in 'password', with: @user.password
+    fill_in 'password-confirmation', with: @user.password
+    fill_in 'first-name', with: @user.first_name_kanji
+    fill_in 'last-name', with: @user.last_name_kanji
+    fill_in 'first-name-kana', with: @user.first_name_kana
+    fill_in 'last-name-kana', with: @user.last_name_kana
+    select @user.birth_day.year, from: 'user[birth_day(1i)]'
+    select @user.birth_day.month, from: 'user[birth_day(2i)]'
+    select @user.birth_day.day, from: 'user[birth_day(3i)]'
+    expect{
+      find('input[name="commit"]').click
+    }.to change { User.count }.by(1)
+    expect(current_path).to eq(root_path)
+  end
 end
---
+++ spec/system/items_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "Items", type: :system do
+  before do
+    @item = FactoryBot.create(:item)
+  end
+
+  context '出品に成功したとき' do
+    it '出品に成功すると、トップページに遷移される' do
+      sign_in(@item.user)
+      expect(page).to have_content('出品する')
+      visit new_item_path
+      image_path = Rails.root.join('public/images/test_image.png') 
+      attach_file('item[image]', image_path, make_visible: true)
+      fill_in "item-name", with: @item.name
+      fill_in "item-info", with: @item.explanation
+      select @item.condition.name, from: "item[condition_id]"
+      select @item.delivery_charge.name, from: "item[delivery_charge_id]"
+      select @item.prefecture.name, from: "item[prefecture_id]"
+      select @item.shipping_day.name, from: "item[shipping_day_id]"
+      select @item.category.name, from: "item[category_id]"
+      fill_in "item-price", with: @item.price
+      profit = @item.price * 0.9
+      add_tax_price = @item.price * 0.1
+      expect(page).to have_content(profit.floor)
+      expect(page).to have_content(add_tax_price.floor)
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Item.count }.by(1)
+      expect(current_path).to eq(root_path)
+    end
+  end
+
+  context '投稿に失敗したとき' do
+    it '商品の投稿に不備があると、商品出品ページに戻される' do
+      sign_in(@item.user)
+      expect(page).to have_content('出品する')
+      visit new_item_path      
+      fill_in "item-name", with: ""
+      fill_in "item-info", with: ""
+      fill_in "item-price", with: ""
+      select "---", from: "item[condition_id]"
+      select "---", from: "item[delivery_charge_id]"
+      select "---", from: "item[prefecture_id]"
+      select "---", from: "item[shipping_day_id]"
+      select "---", from: "item[category_id]"
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Item.count }.by(0)
+      expect(current_path).to eq(items_path)
+    end
+  end
+end
