39c03da7d4eb64c331d517b8452d82a8d125dee2
hachiko38101 <theo21011011@gmail.com>
2022-06-17 17:50:27 +0900

    商品情報編集機能の搭載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -8,12 +8,30 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
   end
 
